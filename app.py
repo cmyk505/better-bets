4183af8ae789d9a746f745e2e0ae69bb95392971
Tristan <tristandavisbrody@gmail.com>
2021-11-06 19:41:28 -0700

    adding back logging for scheduler

--- app.py
+++ app.py
@@ -45,6 +45,8 @@ from flask_apscheduler import APScheduler
 
 import logging
 
+logging.getLogger("apscheduler").setLevel(logging.DEBUG)
+
 from tasks import run_tasks
 
 login_manager = LoginManager()
