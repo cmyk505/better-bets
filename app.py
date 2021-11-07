259d878bc41d499a65d20ab685eee6f422a6444a
Tristan <tristandavisbrody@gmail.com>
2021-11-06 18:32:09 -0700

    -

--- app.py
+++ app.py
@@ -51,14 +51,14 @@ login_manager = LoginManager()
 
 app = Flask(__name__)
 # HEROKU - COMMENT OUT 54-62
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-#     "DATABASE_URL", "SQLALCHEMY_DATABASE_URI"
-# )
-# if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
-#     app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
-#         "SQLALCHEMY_DATABASE_URI"
-#     ].replace("postgres://", "postgresql://", 1)
-# app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "SQLALCHEMY_DATABASE_URI"
+)
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ].replace("postgres://", "postgresql://", 1)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["FLASK_ENV"] = "production"
 # app.config["FLASK_ENV"] = "development"
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI_DEV")
